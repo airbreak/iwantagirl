f2f076206f72428b1721aa1e35579c4da5797869
liwei <vv15802702853@163.com>
2015-11-06 20:47:16 +0800

    game page

--- area/js/home.js
+++ area/js/home.js
@@ -4,6 +4,8 @@
 var Igirl = function ($wrapper) {
     this.$wrapper = $wrapper;
     this.$wrapper.on('click', '#genderBtns img', $.proxy(this, 'startGame'));
+    this.tipsInterval = null;
+    this.gender = null;  //性别
     $('.btn').on('touchstart', function () { });
 };
 
@@ -21,6 +23,36 @@ Igirl.prototype = {
 
     loadingGame: function () {
         this.$wrapper.find('#loadingPage').show().siblings().hide();
+        this.changeTipImg();
+        var that = this;
+        window.setTimeout(function () {
+            that.startGame.call(that);
+        }, 7000);
+    },
+
+    /*开始游戏*/
+    startGame: function () {
+        this.$wrapper.find('#gamePage').show().siblings().hide();
+    },
+
+    /*更换背景图*/
+    changeTipImg: function () {
+        var $img = this.$wrapper.find('#loadingTips img'),
+            flag = true,
+            src='';
+        var that = this;
+
+            that.tipsInterval = window.setInterval(function () {
+                if (flag) {
+                    src = 'imgs/loading/6.png';
+                    flag = false;
+                }else{
+                    src = 'imgs/loading/5.png';
+                    flag = true;
+                }
+                $img.attr('src', src);
+            }, 2000);
+        //},2000);
     },
 
 };
