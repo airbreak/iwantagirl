ee2b2aa03ffbfcf9964c1e92a5be601f6870654e
liwei <vv15802702853@163.com>
2015-11-06 18:53:16 +0800

    home page and loading page

--- area/js/home.js
+++ area/js/home.js
@@ -1,1 +1,26 @@
-﻿
+﻿$(function () {
+    new Igirl($('#wrapper'));
+});
+var Igirl = function ($wrapper) {
+    this.$wrapper = $wrapper;
+    this.$wrapper.on('click', '#genderBtns img', $.proxy(this, 'startGame'));
+    $('.btn').on('touchstart', function () { });
+};
+
+Igirl.prototype = {
+    startGame: function (e) {
+        var $target = $(e.currentTarget),
+            index = $target.index();
+        if (index == 0) { }
+
+        else {
+
+        }
+        this.loadingGame();
+    },
+
+    loadingGame: function () {
+        this.$wrapper.find('#loadingPage').show().siblings().hide();
+    },
+
+};
